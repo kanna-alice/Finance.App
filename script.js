941ad63a2afcb2ae02477d190a814bf794c8853b
Sofia Alice <wallysonm51@gmail.com>
2026-08-12 19:39:23 -0300

    melhorei o app de finanças :3

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-const Contas = []
-
-
+const contas = [];
 function Adicionar() {
-  const button_Add = document.getElementById("button_Add");
-  const input_Add = document.getElementById("input_Add").value;
-
-  const NovaConta = {
-    descriçao: input_Add
-    
-
-  }; 
-   contas.push(novaConta);
+  let input_Nome = document.getElementById("input_Nome").value;
+  let input_Valor = document.getElementById("input_Valor").value;
+  let input_Desc = document.getElementById("input_Desc").value;
+  let input_Categoria = document.getElementById("input_Categoria").value;
+  const Novaconta = {
+    Nome: input_Nome,
+    Valor: input_Valor,
+    Descricao: input_Desc,
+    Categoria: input_Categoria,
+  };
+  if (
+    input_Nome === "" ||
+    input_Valor === "" ||
+    input_Categoria === ""
+  ) {
+    alert("digite algo nos inputs");
+    return(false);
+  } else {
+    alert("Contas adicionadas com sucesso");
+    contas.push(Novaconta);
+    document.getElementById("input_Nome").value = "";
+    document.getElementById("input_Valor").value = "";
+    document.getElementById("input_Desc").value = "";
+    document.getElementById("input_Categoria").value = "";
+    document.getElementById("Add").style.display = "none";
     console.log(contas);
+    renderizarContas();
+  }}
+function renderizarContas() {
+  let html = "";
 
+  contas.forEach((conta) => {
+    html += `
+      <tr>
+        <td>${conta.Nome}</td>
+        <td>${conta.Descricao}</td>
+        <td>${conta.Categoria}</td>
+        <td>${conta.Valor}</td>
+      </tr>
+    `;
+  });
+
+  document.getElementById("financeTableBody").innerHTML = html;
 }
-
 function Menu() {
-    const Menu = document.getElementById("Add");
-    const Open_Menu = document.getElementById("button_Menu");
-     if(Menu.style.display === "block") {
-        Menu.style.display = "none"
-     }else{
-        Menu.style.display = "block"
-     }
-   
-
-    
-
-
-
+  const Menu = document.getElementById("Add");
+  if (Menu.style.display === "block") {
+    Menu.style.display = "none";
+  } else {
+    Menu.style.display = "block";
+  }
 }
