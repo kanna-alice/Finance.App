425684e2d723215846ace039135f1d310bfeeed1
Wallyson Matheus <wallysonm51@gmail.com>
2026-08-17 18:19:33 -0300

    adicao de cauculo nos valores acrecentados

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ function Adicionar() {
   let input_Valor = document.getElementById("input_Valor").value;
   let input_Desc = document.getElementById("input_Desc").value;
   let input_Categoria = document.getElementById("input_Categoria").value;
+
   const Novaconta = {
     Nome: input_Nome,
     Valor: input_Valor,
     Descricao: input_Desc,
     Categoria: input_Categoria,
   };
-  if (
-    input_Nome === "" ||
-    input_Valor === "" ||
-    input_Categoria === ""
-  ) {
+  if (input_Nome === "" || input_Valor === "" || input_Categoria === "") {
     alert("digite algo nos inputs");
-    return(false);
+    return;
   } else {
     alert("Contas adicionadas com sucesso");
     contas.push(Novaconta);
@@ -27,10 +24,11 @@ function Adicionar() {
     document.getElementById("Add").style.display = "none";
     console.log(contas);
     renderizarContas();
-  }}
+  }
+}
 function renderizarContas() {
   let html = "";
-
+  let total = 0;
   contas.forEach((conta) => {
     html += `
       <tr>
@@ -38,10 +36,12 @@ function renderizarContas() {
         <td>${conta.Descricao}</td>
         <td>${conta.Categoria}</td>
         <td>${conta.Valor}</td>
-      </tr>
+        </tr>
     `;
+    let res = document.getElementById("res");
+    total = Number(total) + Number(conta.Valor);
+    res.textContent = `R$: ${total}`;
   });
-
   document.getElementById("financeTableBody").innerHTML = html;
 }
 function Menu() {
